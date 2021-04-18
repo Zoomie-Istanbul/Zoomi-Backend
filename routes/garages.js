1491907d5c22e85dab3148beb0819005f6b556c6
shiranai <regakun836@gmail.com>
2021-04-18 14:35:38 +0900

    tidy up garage routes

--- routes/garages.js
+++ routes/garages.js
@@ -4,11 +4,13 @@ const { authenticate } = require('../middlewares/authMiddleware')
 const Controller = require('../controllers/garageController')
 
 
-router.post('/',Controller.create)
+router.post('/',GarageController.create)
 router.use(authenticate)
-router.put('/', Controller.update)
-router.get('/', Controller.profile)
-
+router.put('/', GarageController.update)
+router.get('/garageProfile', GarageController.profile)
+router.patch('/', GarageController.status)
+router.get('/', GarageController.allGarage)
+router.get('/:id', GarageController.garageDetail)
 
 
 module.exports = router
